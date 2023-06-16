42e1c0ae2605452384c6934aef6dea84172355ac
infitry <leesw504@gmail.com>
2023-06-16 17:04:32 +0900

    feat: webClient 요청 별 타임아웃 테스트

--- src/main/java/com/infitry/laboratory/controller/webclient/WebClientTimeoutController.java
+++ src/main/java/com/infitry/laboratory/controller/webclient/WebClientTimeoutController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClientRequest;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -17,13 +20,42 @@ public class WebClientTimeoutController {
 
     private final WebClient webClient;
 
-    @GetMapping("/read/{seconds}")
+    @GetMapping("/default/read/{seconds}")
     public ResponseEntity<String> readTimeout(@PathVariable String seconds) {
 
         var response = webClient.get()
                 .uri("http://localhost:8080/sleep/" + seconds)
                 .retrieve()
                 .bodyToMono(String.class)
+                .block();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/request/read/{sleepSeconds}/{timeoutSeconds}")
+    public ResponseEntity<String> requestReadTimeout(@PathVariable String sleepSeconds, @PathVariable long timeoutSeconds) {
+
+        var response = webClient.get()
+                .uri("http://localhost:8080/sleep/" + sleepSeconds)
+                .httpRequest(httpRequest -> {
+                    HttpClientRequest reactorRequest = httpRequest.getNativeRequest();
+                    reactorRequest.responseTimeout(Duration.ofSeconds(timeoutSeconds));
+                })
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/reactive/read/{sleepSeconds}/{timeoutSeconds}")
+    public ResponseEntity<String> reactiveReadTimeout(@PathVariable String sleepSeconds, @PathVariable long timeoutSeconds) {
+
+        var response = webClient.get()
+                .uri("http://localhost:8080/sleep/" + sleepSeconds)
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(timeoutSeconds))
                 .block();
 
         return ResponseEntity.ok(response);
