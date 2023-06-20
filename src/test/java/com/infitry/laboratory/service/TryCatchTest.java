2c9bd9e8490dc723f476094b55415a0129dc3daf
infitry <leesw504@gmail.com>
2023-06-20 23:37:33 +0900

    feat: try catch Error, Exception

---
+++ src/test/java/com/infitry/laboratory/service/TryCatchTest.java
@@ -0,0 +1,25 @@
+package com.infitry.laboratory.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TryCatchTest {
+
+    @Test
+    @DisplayName("Exception 과 Error 는 다르다.")
+    public void tryCatchError() {
+        Exception exception = new Exception("default");
+        Error error = new Error("default error");
+        try {
+            throw new Error("error");
+        } catch (Exception e) {
+            exception = e;
+        } catch (Error er) {
+            error = er;
+        }
+        assertEquals("default", exception.getMessage());
+        assertEquals("error", error.getMessage());
+    }
+}
